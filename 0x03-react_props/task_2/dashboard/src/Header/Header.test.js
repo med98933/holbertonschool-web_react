a597ad3b96eeaf3317e098f7534a0e0c81bbaa53
med98933 <m.ladhari@outlook.com>
2022-03-07 13:55:33 +0100

    task 2

---
+++ 0x03-react_props/task_2/dashboard/src/Header/Header.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Enzyme from "enzyme";
+
+Enzyme.configure({adapter: new adapter()});
+
+
+it('checks header rendering', () => {
+    expect(Enzyme.shallow(<Header />)).to.have.lengthOf(1);
+});
+
+it('checks image rendering', () => {
+    expect(Enzyme.shallow(<Header />).find('img')).to.have.lengthOf(1);
+})
