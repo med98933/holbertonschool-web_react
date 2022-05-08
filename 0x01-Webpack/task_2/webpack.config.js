0d81f7b7e373d122318bede75e98728a077532b9
med98933 <m.ladhari@outlook.com>
2022-05-08 16:37:41 +0100

    task2

---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  mode: 'production',
+  output: {
+
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+
+}
